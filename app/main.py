770725048503f7e75351fa4a633b3b743ad0b013
ElderGut <144392085+ElderGut@users.noreply.github.com>
2026-07-05 23:45:44 -0600

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,1 +1,51 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import joblib
+import numpy as np
 
+app = FastAPI(
+    title="API de Clasificación de Cáncer de Mama",
+    description="API para clasificar tumores como benignos o malignos usando Logistic Regression.",
+    version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = joblib.load("app/model.pkl")
+
+class TumorData(BaseModel):
+    features: list[float]
+
+@app.get("/")
+def home():
+    return {
+        "message": "API funcionando correctamente",
+        "endpoints": ["/predict", "/docs"]
+    }
+
+@app.post("/predict")
+def predict(data: TumorData):
+    if len(data.features) != 30:
+        return {
+            "error": "El modelo requiere exactamente 30 variables numéricas."
+        }
+
+    input_data = np.array(data.features).reshape(1, -1)
+
+    prediction = model.predict(input_data)[0]
+    probability = model.predict_proba(input_data)[0].max()
+
+    label = "Benigno" if prediction == 1 else "Maligno"
+
+    return {
+        "prediction": int(prediction),
+        "label": label,
+        "confidence": round(float(probability), 4)
+    }
--- app/train_model.py
+++ app/train_model.py
@@ -1,1 +1,24 @@
+import joblib
+import pandas as pd
 
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+data = load_breast_cancer()
+
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = data.target
+
+model = Pipeline([
+    ("scaler", StandardScaler()),
+    ("classifier", LogisticRegression(C=0.1, solver="lbfgs", max_iter=5000, random_state=42))
+])
+
+model.fit(X, y)
+
+joblib.dump(model, "app/model.pkl")
+
+print("Modelo guardado en app/model.pkl")
